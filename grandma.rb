9652201fda4d0bb1b6b8fbdb5b1449ba27196b91
Jennifer Dobak <jdobak1@gmail.com>
2018-01-09 19:01:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,4 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
-end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
@@ -13,3 +11,6 @@ end
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma
+  
+end
