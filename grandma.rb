f7e433c155afbc78a8b2b094a51d70c674c150a6
Jennifer Dobak <jdobak1@gmail.com>
2018-01-09 19:11:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,6 +11,7 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+require 'pry'
 def speak_to_grandma (phrase)
   if phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
@@ -21,5 +22,3 @@ def speak_to_grandma (phrase)
   end
   binding.pry
 end
-
-binding.pry
