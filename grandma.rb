2562e68e2f8e1b99d65a49a12da7d076a00c1801
Jennifer Dobak <jdobak1@gmail.com>
2018-01-09 19:13:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 require 'pry'
 def speak_to_grandma (phrase)
   if phrase != phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   else phrase ==  "I LOVE YOU GRANDMA!"
